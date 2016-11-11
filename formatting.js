0c2cba70cd45e80b794be482c1f4bcaf1905dfa2
themnmoores <themnmoores@gmail.com>
2016-11-11 15:51:54 -0600

    Fixed formatting issue on android

--- formatting.js
+++ formatting.js
@@ -5,7 +5,7 @@
 //
 
 //
-// Displays a header bar at the top of a web page.
+// Displays a header bar at the top of a web page, and adds a favicon
 // *** HTML must create a <div id="headerTopBar"></div> before calling.
 //
 //    centeredText    Text to display centered in the bar towards to the bottom of the bar (used to indicate the page we are on)
@@ -16,12 +16,18 @@ function commonPageHeaderBar(centeredText, pathToBase) {
       theDivWeWant.style.position="absolute";
       theDivWeWant.style.left="0px";
       theDivWeWant.style.top="0px";
-      theDivWeWant.style.width="100%";
       theDivWeWant.style.height ="160px";
       theDivWeWant.style.backgroundColor="#862507";
       theDivWeWant.style.border="0px none";
+      deviceWidth = window.innerWidth - window.pageXOffset;
+      if (deviceWidth < screen.width)
+        deviceWidth = screen.width;
+      window.alert("width = " + deviceWidth.toString());
+      theDivWeWant.style.width=deviceWidth.toString() + "px";
       theDivWeWant.style.minWidth="1024px";
+      //theDivWeWant.style.minWidth="1200px";
       theDivWeWant.innerHTML = '<a href="' + pathToBase + 'index.html"> <p style="margin:0px 0px 0px 0px;position:absolute;left:20px;width:30%;top:20px;font-family:Georgia, serif;font-size: 40px;color:white">THEMNMOORES.NET</p> </a><img src="' + pathToBase + 'pictures/familysmall.jpg" alt="Family" height="160" style="position:relative;left:85%;top:0px"><p style="margin:0px 0px 0px 0px;position:relative;right;left:0%;width:100%;top:-60px;text-align: center;font-family:Georgia, serif;font-size: 40px;color:white">' + centeredText +'</p>';
+      theDivWeWant.innerHTML += '<link rel="icon" type="image/x-icon" href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/v7+AP///wCXl5cAZmZmAJ2dnQB9fX0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgIAAgACAgICAgICAgIAIgICAgICAgACAAIEBAQCAgICAgAEAAIAAiIiRERCIiIiIiJEREIiIiIiIkREQiIiIiIiRERCIiIiIiJEREQSIiIiIkREREQiIiAjBAQAAiIiICUGAgIiIiIgIgACACIiIiAiAgICIiIiAAICAgACIiIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" >'
 }
 
 
