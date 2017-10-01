df0e526a58c536c95a7d7e92fb9dc75ca2e13c84
themnmoores <themnmoores@gmail.com>
2017-10-01 10:45:08 -0500

    First commit of photo viewer

---
+++ Photos/Sawtooths2017/photofilelist.js
@@ -0,0 +1,42 @@
+
+var photoFileListJSONString;
+
+function populatePhotoListJSONString()
+{
+  photoFileListJSONString = '{';
+  photoFileListJSONString +=' "displayWindow": {"width": 800,"height": 600,"title": "Sawtooths 2017"},';
+  photoFileListJSONString +=' "images": [';
+  photoFileListJSONString +=' {"src": "1.jpg","caption": "Day 1: Iron Creek trailhead to Lake 8271 south of Sawtooth Lake."},';
+  photoFileListJSONString +=' {"src": "2.jpg","caption": "Day 1: The view as we approached Stanley from Boise on highway 21"},';
+  photoFileListJSONString +=' {"src": "3.jpg","caption": "Day 1: Into the wilderness."},';
+  photoFileListJSONString +=' {"src": "4.jpg","caption": "Day 1: First trail junction, being a Sunday there were many folks on the trail."},';
+  photoFileListJSONString +=' {"src": "5.jpg","caption": "Day 1"},';
+  photoFileListJSONString +=' {"src": "6.jpg","caption": "Day 1: Darrell and Vince heading up to Alpine Lake"},';
+  photoFileListJSONString +=' {"src": "7.jpg","caption": "Day 1: First wildflower meadow (of many to come)"},';
+  photoFileListJSONString +=' {"src": "8.jpg","caption": "Day 1: First wildflower meadow"},';
+  photoFileListJSONString +=' {"src": "9.jpg","caption": "Day 1: First wildflower meadow"},';
+  photoFileListJSONString +=' {"src": "10.jpg","caption": "Day 1: David donned his poncho with the first rain."},';
+  photoFileListJSONString +=' {"src": "11.jpg","caption": "Day 1: Break out the cameras the sun is peeking through"},';
+  photoFileListJSONString +=' {"src": "12.jpg","caption": "Day 1: Alpine Lake is what was being photographed"},';
+  photoFileListJSONString +=' {"src": "13.jpg","caption": "Day 1: A bit more sunshine brings the cameras out"},';
+  photoFileListJSONString +=' {"src": "14.jpg","caption": "Day 1: This must be why they call them the Sawtooths, every ridgeline seemed to have this jagged appearance."},';
+  photoFileListJSONString +=' {"src": "15.jpg","caption": "Day 1: Still smiling after 1800 feet of climbing."},';
+  photoFileListJSONString +=' {"src": "16.jpg","caption": "Day 1: Sawtooth Lake outlet creek"},';
+  photoFileListJSONString +=' {"src": "17.jpg","caption": "Day 1: Making sure David makes it over the first water crossing."},';
+  photoFileListJSONString +=' {"src": "18.jpg","caption": "Day 1: On to Sawtooth lake and beyond almost to North Baron Creek"},';
+  photoFileListJSONString +=' {"src": "19.jpg","caption": "Day 1: Sawtooth lake and Mount Regan after a brief rain shower came through"},';
+  photoFileListJSONString +=' {"src": "20.jpg","caption": "Day 1: Trailside flowers"},';
+  photoFileListJSONString +=' {"src": "21.jpg","caption": "Day 1: Darrell handling the first snow field of the trip."},';
+  photoFileListJSONString +=' {"src": "22.jpg","caption": "Day 1: Looking down at Lake 8271"},';
+  photoFileListJSONString +=' {"src": "23.jpg","caption": "Day 1: Hardie plant in the trail"},';
+  photoFileListJSONString +=' {"src": "24.jpg","caption": "Day 1: I think the left most peak is Baron peak, heading past the lakes to the outlet creek."},';
+  photoFileListJSONString +=' {"src": "25.jpg","caption": "Day 1: Peak 10330 on the left and Peaks 9464 and 9656 on the right (a lot of lakes and preaks are only known by their elevation)"},';
+  photoFileListJSONString +=' {"src": "26.jpg","caption": "Day 1: Backside of Mount Regan from our campsite on the outlet creek."},';
+  photoFileListJSONString +=' {"src": "27.jpg","caption": "Day 1: Nothing like a package of freezed dried food after a day on the trail. At least the rain held off until we had eaten! "},';
+  photoFileListJSONString +=' {"src": "28.jpg","caption": "Day 2: Our original plan was to hike back out and car camp for a night, but we thought it too scenic around Sawtooth Lake for that and modified our plan to do a short day back to Alpine Lake for the night. Steve and I decided to drop our packs at Sawtooth Lake and wander up the pass towards McGown Lakes and see over the other side. The sun started to come out in the early afternoon making for some incrediable views of Sawtooth Lake."},';
+  photoFileListJSONString +=' {"src": "29.jpg","caption": "Day 2: A foggy start with the clouds hanging on the peaks."},';
+  photoFileListJSONString +=' {"src": "30.jpg","caption": " "},';
+  
+  photoFileListJSONString +=' {"src": "196.jpg","caption": "last2image"}';
+  photoFileListJSONString +=' ]}';
+}
---
+++ photoslideshow.js
@@ -0,0 +1,143 @@
+//
+// Copyright 2016-2017, Richard J. Moore all rights reserved
+//
+// photoslideshow.js
+//
+// Collection of common functions for doing a photo slide show
+//
+// A file containing the JSON string photoFileListJSONString must be src'd in the HTML file
+// e.g.
+//      <script src="photofilelist.js"></script>
+//
+
+// Some globals setup by the initializePhotoSlideShow, which must be called first
+
+var currentImage;
+var currentImageNumberInSlideShow;
+var photoSlideShow;
+var caption;
+
+var leftArrow;
+var rightArrow;
+
+
+function initializePhotoSlideShow(canvasID, titleID, captionID, leftArrowID, rightArrowID)
+{
+  populatePhotoListJSONString();
+  photoSlideShow = JSON.parse(photoFileListJSONString);
+
+  theCanvas = document.getElementById(canvasID);
+  theCanvas.height=Number(photoSlideShow.displayWindow.height);
+  theCanvas.width=Number(photoSlideShow.displayWindow.width);
+  theCanvas.style.borderWidth="3px";
+  theCanvas.style.borderStyle = "solid";
+  theCanvas.style.borderColor="#000000";
+
+  caption = document.getElementById(captionID);
+  caption.style.width = theCanvas.width.toString() + "px";
+  leftArrow =  document.getElementById(leftArrowID);
+  rightArrow =  document.getElementById(rightArrowID);
+  
+  
+  
+  theTitle = document.getElementById(titleID);
+  theTitle.innerHTML = '<b>' + photoSlideShow.displayWindow.title + '</b>';
+  
+  currentImageNumberInSlideShow = 0;
+  setCurrentImage(photoSlideShow.images[currentImageNumberInSlideShow].src,photoSlideShow.images[currentImageNumberInSlideShow].caption);
+}
+
+
+function setCurrentImage(imageFileName, captionText)
+{
+  caption.innerHTML = captionText;
+  currentImage = new Image();
+  currentImage.onload = displayCurrentImage;
+  currentImage.src = imageFileName;
+}
+
+
+function displayCurrentImage()
+{
+  ctx = theCanvas.getContext("2d");
+  ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+  // Figure out how to display
+  var x = 0;
+  var y = 0;
+  var width = ctx.canvas.width;
+  var height = ctx.canvas.height;
+  
+  // Figure out how to fit the image to the slide show
+  scaleFactorX = width / currentImage.width;
+  scaleFactorY = height / currentImage.height;
+  if (scaleFactorX < scaleFactorY)      // Means the x dimension gets scaled more
+  {
+    y = (height - (currentImage.height * scaleFactorX)) / 2.0; // So we center the image vertically
+    height = currentImage.height * scaleFactorX;
+  }
+  else                                  // Means the y dimension gets scaled more
+  {
+    x = (width - (currentImage.width * scaleFactorY)) / 2.0; // So we center the image horizontally
+    width = currentImage.width * scaleFactorY;
+    
+  }
+  
+  ctx.drawImage(currentImage, x, y, width, height);
+  
+}
+
+//
+// Callback for the right arrow being pressed, goes to the next higher (N+1) image in the list
+// If we are at the end of the list circles back to image 0
+//
+
+function rightArrowPressed()
+{
+  if (currentImageNumberInSlideShow == photoSlideShow.images.length-1)
+  {
+    //return;         // Nothing to do we are alread at the end of the list (really an error)
+    currentImageNumberInSlideShow = 0;
+  }
+  else
+  {
+    currentImageNumberInSlideShow++;
+  }
+  
+  leftArrow.style.visibility = "visible";
+  // if (currentImageNumberInSlideShow == photoSlideShow.images.length-1)
+  // {
+  //   rightArrow.style.visibility = "hidden";
+  // }
+  
+  setCurrentImage(photoSlideShow.images[currentImageNumberInSlideShow].src,photoSlideShow.images[currentImageNumberInSlideShow].caption);
+  displayCurrentImage();
+}
+
+
+
+//
+// Callback for the left arrow being pressed, goes to the next lower (N-1) image in the list
+// If we are at the start of the list circles back to image N
+//
+
+function leftArrowPressed()
+{
+  if (currentImageNumberInSlideShow === 0)
+  {
+    //return;         // Nothing to do we are alread at the end of the list (really an error)
+    currentImageNumberInSlideShow = photoSlideShow.images.length-1;
+  }
+  else
+  {
+    currentImageNumberInSlideShow--;
+  }
+  
+  rightArrow.style.visibility = "visible";
+  // if (currentImageNumberInSlideShow === 0)
+  // {
+  //   leftArrow.style.visibility = "hidden";
+  // }
+  
+  setCurrentImage(photoSlideShow.images[currentImageNumberInSlideShow].src,photoSlideShow.images[currentImageNumberInSlideShow].caption);
+  displayCurrentImage();
+}
